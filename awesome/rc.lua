b9004f4535f9a9dfc1b25c975af3d282f47b8138
Weiberle17 <maxweiberle63@gmail.com>
2022-12-14 15:50:35 +0100

    formatting

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -82,6 +82,7 @@ client.connect_signal("manage", function (c)
     awful.placement.no_offscreen(c)
   end
 end)
+
 -- Window Transparancy
 client.connect_signal("focus", function (c)
   c.border_color = beautiful.border_focus
